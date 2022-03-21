e0de104346457ad57b648c5dd9cd73f40262fe0a
BarKirly <kiralybar95@gmail.com>
2022-03-21 15:58:59 +0200

    Daily Challenge

--- Week2/Day2/Daily Challenge/DailyCha.js
+++ Week2/Day2/Daily Challenge/DailyCha.js
@@ -1,4 +1,21 @@
-let sentence = "The movie is not that bad, I like it";
+let sen = "The movie is not that bad I like it";
 
-let wordNot = sentence[3][0];
-console.log(wordNot);
+const arr = sen.split(" ");
+
+let wordNot = arr.indexOf("not");
+let wordBad = arr.indexOf("bad");
+console.log(wordNot, wordBad);
+arr.splice(wordNot, wordBad - wordNot + 1, "good");
+console.log(arr);
+console.log(arr.join(" "));
+// let wordNot = sen.indexOf("not");
+// let wordBad = sen.indexOf("bad");
+
+// let sen1 = sen.substring(0, wordNot);
+// let sen2 = sen.substring(wordBad + 4, sen.length);
+
+// if (wordBad > wordNot) {
+//   console.log(sen1 + "good" + sen2);
+// } else {
+//   console.log(sen);
+// }
