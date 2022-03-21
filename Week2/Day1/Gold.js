194072d6363cafa476f0ce0655d3e7eed7116af2
BarKirly <kiralybar95@gmail.com>
2022-03-21 14:18:01 +0200

    Week 2 Day 2 Ex2

---
+++ Week2/Day1/Gold.js
@@ -0,0 +1,19 @@
+// // Exercise 1
+
+// let sentence = ["my", "favorite", "color", "is", "blue"];
+
+// console.log(sentence.join(" "));
+
+// // Exercise 2
+
+// let str1 = "mix";
+// let str2 = "pod";
+
+// let arr1 = str1.split("");
+// let arr2 = str2.split("");
+
+// console.log(arr1, arr2);
+// let str3 = arr1[0] + arr2.splice(1, 2).join("");
+// console.log(str3);
+// let str4 = arr2[0] + arr1.splice(1, 2).join("");
+// console.log(str4);
---
+++ Week2/Day2/script.js
@@ -0,0 +1,39 @@
+// let userCart = {
+//   username: "John",
+//   password: 1234,
+//   isUserSignedIn: true,
+//   cart: {
+//     apple: 2,
+//     banana: 1,
+//     pear: 5,
+//   },
+//   prices: {
+//     apple: 0.5,
+//     banana: 0.8646466363,
+//     pear: 0.2,
+//   },
+// };
+
+// userCart["lastName"] = "Smith";
+
+// userCart["prices"]["pear"] *= 1.17;
+// console.log(userCart);
+
+// let userFruit = prompt(
+//   "Which fruit would you like: Banana / Apple / Pear?"
+// ).toLowerCase();
+// console.log(userFruit);
+// console.log(`The price is ${userCart["prices"][userFruit]}`);
+
+// let player = {
+//   username: "Jane",
+//   age: 22,
+//   levelGame: 40,
+//   products: ["gun", "cat", "uniform"],
+// };
+
+// if (player["levelGame"] >= 50) {
+//   console.log((player["isGoodPlayer"] = "good"));
+// } else if (player["levelGame"] > 15 && player["levelGame"] < 50) {
+//   console.log((player["isGoodPlayer"] = "middle"));
+// } else console.log((player["isGoodPlayer"] = "bad"));
